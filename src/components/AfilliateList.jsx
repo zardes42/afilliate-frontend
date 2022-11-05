1ac1816aff7e041bca15ee8bb258104b2423d219
Iyinoluwa <iyinoyelz69@gmail.com>
2022-11-04 19:09:07 -0600

    removes unused variable

--- src/components/AfilliateList.jsx
+++ src/components/AfilliateList.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect,useState,useRef} from 'react'
+import React,{useEffect,useState} from 'react'
 import styled from 'styled-components'
 import {DefaultHead,DefaultTitle,DefaultContent,DefaultBadge} from '../styles/styles'
 import {axiosAuth} from '../axios'
